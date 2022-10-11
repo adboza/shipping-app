0083dd29ec851c3f868a38018f8a3387b7a7f90b
Adriana Boza <adboza@gmail.com>
2022-10-11 15:19:03 -0300

    testes unitarios veículo ok

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,9 @@
 class Vehicle < ApplicationRecord
   belongs_to :vehicle_type
+  validates :licence_plate, :status, :year, :load_capacity, :vehicle_type_id , presence: true
+  validates :load_capacity, numericality: {greater_than_or_equal_to: 0}
+  validates :load_capacity, numericality: true
+  validates :licence_plate, uniqueness: true, length: { is: 7 }
 
   enum status: { unavailable: 0, on_delivery: 5, available: 9 }
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,5 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-  
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when licence_plate is empty' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        vehicle = Vehicle.new(licence_plate: '', status: :unavailable, year: 2.days.ago, vehicle_type: type, load_capacity: 300)
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      
+      it 'false when year is empty' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        vehicle = Vehicle.new(licence_plate: 'ABC1100', status: :unavailable, year: nil, vehicle_type: type, load_capacity: 300)
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when vehicle_type is empty' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        vehicle = Vehicle.new(licence_plate: 'ABC1100', status: :unavailable, year: 2.days.ago, vehicle_type: nil, load_capacity: 300)
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when load_capacity is empty' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        vehicle = Vehicle.new(licence_plate: 'ABC1100', status: :unavailable, year: 2.days.ago, vehicle_type: type, load_capacity: nil)
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when load capacity is not bigger than zero' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        vehicle = Vehicle.new(licence_plate: 'ABC1100', status: :unavailable, year: 2.days.ago, vehicle_type: type, load_capacity: -300)
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when load capacity is not numeric field' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        vehicle = Vehicle.new(licence_plate: 'ABC1100', status: :unavailable, year: 2.days.ago, vehicle_type: type, load_capacity: 'a')
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when licence plate is not unique' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        Vehicle.create!(licence_plate: 'ABC1100', status: :available, year: 1.year.ago, vehicle_type: type, load_capacity: 360)
+        vehicle = Vehicle.new(licence_plate: 'ABC1100', status: :unavailable, year: 2.days.ago, vehicle_type: type, load_capacity: 300)
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+
+      it 'false when licence plate is not 7 digits long' do
+        #Arrange
+        type = VehicleType.create!(vehicle_type: :utility_vehicle)
+        vehicle = Vehicle.new(licence_plate: 'ABC100', status: :unavailable, year: 2.days.ago, vehicle_type: type, load_capacity: 300)
+        #Act
+        result = vehicle.valid?
+        #Assert
+        expect(result).to eq false
+      end
+      
+      # it 'false when load weight is less than zero' do
+      #   #Arrange
+      #   modality = DeliveryModality.create!(mod_name: 'Expressa', mod_price: 4)
+      #   load = LoadCategory.new(min_weight: -2, max_weight: 4, weight_price: 1, delivery_modality_id: modality.id)
+      #   #Act
+      #   result = load.valid?
+      #   #Assert
+      #   expect(result).to eq false
+      # end
+
+    end
+  end
 end
